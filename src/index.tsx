d7d79a6ce47d06a782481150a4a27b773df242fb
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-04-12 20:24:03 -0300

    feat: add functions to acess native functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 const ReactNativeIrManage = require('./NativeReactNativeIrManage').default;
 
-export function multiply(a: number, b: number): number {
-  return ReactNativeIrManage.multiply(a, b);
+export function hasIrEmitter(): Promise<boolean> {
+  return ReactNativeIrManage.hasIrEmitter();
+}
+
+export function transmit(
+  frequency: number,
+  pattern: number[]
+): Promise<boolean> {
+  return ReactNativeIrManage.transmit(frequency, pattern);
+}
+
+export function getCarrierFrequencies(): Promise<{
+  maxFrequency: number;
+  minFrequency: number;
+}> {
+  return ReactNativeIrManage.getCarrierFrequencies();
 }
