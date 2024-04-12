26119a93a6d646b479f801ba4ad4ed2e38f54a7e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-04-12 20:15:19 -0300

    feat: update android folder

--- android/src/main/java/com/lucas/reactnativeirmanage/ReactNativeIrManageModule.java
+++ android/src/main/java/com/lucas/reactnativeirmanage/ReactNativeIrManageModule.java
@@ -2,15 +2,24 @@ package com.lucas.reactnativeirmanage;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.annotations.ReactModule;
+import android.hardware.ConsumerIrManager;
+import android.hardware.ConsumerIrManager.CarrierFrequencyRange;
+import static android.content.Context.CONSUMER_IR_SERVICE;
 
 @ReactModule(name = ReactNativeIrManageModule.NAME)
 public class ReactNativeIrManageModule extends NativeReactNativeIrManageSpec {
   public static final String NAME = "ReactNativeIrManage";
-
+  private final ConsumerIrManager manager;
   public ReactNativeIrManageModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    manager = (ConsumerIrManager) reactContext.getSystemService(CONSUMER_IR_SERVICE);
   }
 
   @Override
@@ -23,7 +32,42 @@ public class ReactNativeIrManageModule extends NativeReactNativeIrManageSpec {
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @Override
-  public double multiply(double a, double b) {
-    return a * b;
+  public void hasIrEmitter(Promise promise) {
+        promise.resolve(manager.hasIrEmitter());
+    }
+
+  @Override
+  public void transmit(double carrierFrequency,  ReadableArray burstsPattern, Promise promise) {
+    int[] pattern = new int[burstsPattern.size()];
+    int value = (int)carrierFrequency;
+    for (int i = 0; i < burstsPattern.size(); i++) {
+      pattern[i] = burstsPattern.getInt(i);
+    }
+    try {
+      manager.transmit(value, pattern);
+      promise.resolve(true);
+    } catch (Exception e) {
+      promise.reject(e);
+    }
+  }
+
+  @Override
+  public void getCarrierFrequencies(Promise promise) {
+      try {
+          CarrierFrequencyRange[] carrierFrequencyRanges = manager.getCarrierFrequencies();
+          WritableArray carrierFrequencies = Arguments.createArray();
+
+          for (CarrierFrequencyRange carrierFrequencyRange : carrierFrequencyRanges) {
+              WritableMap carrierFrequency = Arguments.createMap();
+              carrierFrequency.putInt("minFrequency", carrierFrequencyRange.getMinFrequency());
+              carrierFrequency.putInt("maxFrequency", carrierFrequencyRange.getMaxFrequency());
+              carrierFrequencies.pushMap(carrierFrequency);
+          }
+
+          promise.resolve(carrierFrequencies);
+      } catch (Exception e) {
+          promise.reject(e);
+      }
+
   }
 }
