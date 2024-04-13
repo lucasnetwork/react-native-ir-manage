274b7bf3a5055b2cce930ea514518c3cc180331a
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-04-13 19:38:20 -0300

    fix: file of android

--- android/src/main/java/com/lucas/reactnativeirmanage/ReactNativeIrManageModule.java
+++ android/src/main/java/com/lucas/reactnativeirmanage/ReactNativeIrManageModule.java
@@ -31,12 +31,12 @@ public class ReactNativeIrManageModule extends NativeReactNativeIrManageSpec {
 
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
-  @Override
+  @ReactMethod
   public void hasIrEmitter(Promise promise) {
         promise.resolve(manager.hasIrEmitter());
     }
 
-  @Override
+  @ReactMethod
   public void transmit(double carrierFrequency,  ReadableArray burstsPattern, Promise promise) {
     int[] pattern = new int[burstsPattern.size()];
     int value = (int)carrierFrequency;
@@ -51,7 +51,7 @@ public class ReactNativeIrManageModule extends NativeReactNativeIrManageSpec {
     }
   }
 
-  @Override
+  @ReactMethod
   public void getCarrierFrequencies(Promise promise) {
       try {
           CarrierFrequencyRange[] carrierFrequencyRanges = manager.getCarrierFrequencies();
